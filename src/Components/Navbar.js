8102ad5fca0c0178f34a2afd0d68c8c057bd2811
apton-jeevan <jeevan.kurian@aptonworks.com>
2023-03-01 21:35:32 +0530

    mobNav stared

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -132,6 +132,7 @@ export default function Navbar() {
 
 
   return (
+    <>
     <nav className="myNavbar">
      <Link to="/" ><img className="brand-logo-img" src="https://www.tcs.com/content/dam/global-tcs/en/images/home/dark-theme.svg" alt="Tata Consultancy Services" title="Tata Consultancy Services" /></Link> 
       <ul className="myNavLinkSet">
@@ -621,5 +622,31 @@ export default function Navbar() {
       </ul>
 
     </nav>
+
+  <nav className="myMobNav navbar navbar-expand-dark bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+     
+    </div>
+  </div>
+</nav>
+    </>
+
+    
   )
 }
