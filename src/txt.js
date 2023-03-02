1a884f0f3d5766aa65ade4124fd9a6dab79e8c76
apton-jeevan <jeevan.kurian@aptonworks.com>
2023-03-02 05:44:32 +0530

    navbar working added rightNavLinks mar2 5:45am

--- src/txt.js
+++ src/txt.js
@@ -1,22 +1,4 @@
-<nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-     
-    </div>
-  </div>
-</nav>
+<span class="navbar-text d-flex" tabindex="0" data-target="#searchModal" data-toggle="modal" data-backdrop="static" role="button">
+  <span class="search-icon" title="Search"></span>
+  <span class="sr-only">Search</span>
+</span>
