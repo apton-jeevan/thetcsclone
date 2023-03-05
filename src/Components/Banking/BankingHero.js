217e6cd8e3c955c047a6c558982a9fddc0493968
apton-jeevan <jeevan.kurian@aptonworks.com>
2023-03-05 17:10:25 +0530

    banking hero done mar5 5:15pm

--- src/Components/Banking/BankingHero.js
+++ src/Components/Banking/BankingHero.js
@@ -3,8 +3,18 @@ import "./BankingHero.css"
 
 export default function BankingHero() {
     return (
-        <div>
-            
+        <div className='bankingHero'>
+            <div className="container">
+                <div className="bankingHeroContent">
+                    <p className='head'>Industries/Banking</p>
+                    <div className="bankingHeroText col-md-12 col-lg-12 col-xl-5">
+                        <h1>Banks go boundaryless</h1>
+                        <p className='para'>We help you drive holistic growth with a secure, boundaryless, and humane approach.</p>
+                        <a className="letstalk" href="https://www.tcs.com/contact-us/industry-overlay/talk-to-an-expert-banking" >Let's talk <i class="fa-solid fa-circle-arrow-right letstalkIcon"></i></a>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
--- src/txt.js
+++ src/txt.js
@@ -1,1 +1,12 @@
-Click here<i class="fa-solid fa-circle-arrow-right"></i></a>
+<div class="hero-banner-content-div">
+
+    <div class="hero-banner-title-div pt-lg-32 pb-lg-3 rte-links-div ">
+        <h1>Banks go boundaryless</h1>
+
+        <p>We help you drive holistic growth with a secure, boundaryless, and humane approach.</p>
+
+    </div>
+
+    <a href="https://www.tcs.com/contact-us/industry-overlay/talk-to-an-expert-banking" >Let's talk <i class="fa-solid fa-circle-arrow-right"></i></a>
+
+</div>
