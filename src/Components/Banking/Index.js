a5ae97143247b360166e83187215edda44745972
apton-jeevan <jeevan.kurian@aptonworks.com>
2023-03-05 19:18:36 +0530

    completed infoucs section

--- src/Components/Banking/Index.js
+++ src/Components/Banking/Index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import BankingHero from './BankingHero'
+import Belief from './Belief'
 
 export default function Index() {
   return (
     <div>
      <BankingHero/>
+     <Belief/>
     </div>
   )
 }
---
+++ src/Components/Banking/inFocusData.js
@@ -0,0 +1,30 @@
+export const data = [
+    {
+        id : 1,
+        title: "The open banking solution for gig workers",
+        date: "Blog | 19 Dec 2022",
+        imgUrl: "https://s7ap1.scene7.com/is/image/TCSCOMprod/open-banking-customized-solutions-gig-workers-Web?wid=2000&hei=788&dpr=off",
+        cardUrl: "https://www.tcs.com/insights/blogs/open-banking-customized-solutions-gig-workers"
+    },
+    {
+        id: 2,
+        title: "Cloud adoption in European financial institutions",
+        date: "White Paper | 21 Dec 2022",
+        imgUrl: "https://s7ap1.scene7.com/is/image/TCSCOMprod/bank-payment-web?wid=2000&hei=788&dpr=off",
+        cardUrl: "https://www.tcs.com/what-we-do/industries/banking/white-paper/cloud-technologies-adoption-financial-institutions"
+    },
+    {
+        id: 3,
+        title: "The new wholesale payments paradigm",
+        date: "White Paper | 13 Jan 2023",
+        imgUrl: "https://www.tcs.com/content/dam/global-tcs/en/images/who-we-are/iron-mountain-Web-3840x1512.jpg/jcr:content/renditions/cq5dam.thumbnail.1280.765.png",
+        cardUrl: "https://www.tcs.com/what-we-do/industries/banking/white-paper/banks-payments-transformation-strategies"
+    },
+    {
+        id: 4,
+        title: "Legacy modernization in banks",
+        date: "White Paper | 06 Sep 2022",
+        imgUrl: "https://s7ap1.scene7.com/is/image/TCSCOMprod/gartner-magic-Web-3840x1512-1?wid=2000&hei=788&dpr=off",
+        cardUrl: "https://www.tcs.com/what-we-do/industries/banking/white-paper/legacy-modernization-microservices-architecture"
+    }, 
+]
