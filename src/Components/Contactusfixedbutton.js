0391e17127ff9a08a563103057a80a1058254efa
apton-jeevan <jeevan.kurian@aptonworks.com>
2023-03-08 10:31:37 +0530

    slide button

--- src/Components/Contactusfixedbutton.js
+++ src/Components/Contactusfixedbutton.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import "./Contactusfixedbutton.css"
+import { useEffect } from 'react';
 
 export default function Contactusfixedbutton() {
     let slideButton = document.querySelector('.slide-button');
 
-window.addEventListener('scroll', function() {
-  if (window.pageYOffset > 0) {
-    slideButton.classList.add('out');
-    slideButton.classList.remove('in');
-  } else {
-    slideButton.classList.add('in');
-    slideButton.classList.remove('out');
-  }
-});
+useEffect(() => {
+    window.addEventListener('scroll', function() {
+        if (window.pageYOffset > 0) {
+          slideButton.classList.add('out');
+          slideButton.classList.remove('in');
+        } else {
+          slideButton.classList.add('in');
+          slideButton.classList.remove('out');
+        }
+      });
+}, [])
+
+
+
     return (
         <div>
             <div className="button-container">
